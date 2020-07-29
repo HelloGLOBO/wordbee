28d265db8b2cdce95f96962432dab951639f57a2
Robert Peralta <robert.peralta@helloglobo.com>
2020-07-29 09:08:40 -0400

    Add some specs

--- spec/wordbee_spec.rb
+++ spec/wordbee_spec.rb
@@ -1,9 +1,24 @@
 RSpec.describe Wordbee do
+	ACCOUNT_ID = "globotesting"
+	PASSWORD = "8DOhgcwWDNW7iwZc"
+
   it "has a version number" do
     expect(Wordbee::VERSION).not_to be nil
-  end
+	end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+	it 'should connect' do
+		client = Wordbee::API::Client.new(ACCOUNT_ID, PASSWORD)
+		auth_token = client.connect
+		expect(auth_token).not_to be_nil
+	end
+
+	xit 'should connect' do
+		# client = Wordbee::API::Client.new(ACCOUNT_ID, PASSWORD) do |client|
+		# 	auth_token = client.send(:auth_token)
+		# 	expect(auth_token).not_to be_nil
+		# end
+		#
+		# auth_token = client.send(:auth_token)
+    # expect(auth_token).to be_nil
   end
 end
